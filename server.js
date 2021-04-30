e4d286c2bafbcd8d27888191d3351105728f6d0e
junghyuni <anjjang23@gmail.com>
2021-04-30 17:33:37 +0900

    architecture

--- server.js
+++ server.js
@@ -1,53 +1,10 @@
-import { PrismaClient } from ".prisma/client";
-import { ApolloServer, gql } from "apollo-server";
-
-const client = new PrismaClient()
-
-const typeDefs = gql`
-  type Movie {
-    id: Int!
-    year: Int!
-    title: String!
-    genre: String
-    createdAt: String!
-    updatedAt: String!
-  }
-
-  type Query {
-    movies: [Movie]
-    movie(id:Int!): Movie
-  }
-
-  type Mutation {
-    createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(id: Int!): Movie
-    updateMovie(id: Int!, year: Int!): Movie
-  }
-`;
-
-const resolvers = {
-  Query: {
-    movies: () => client.movie.findMany(),
-    movie: (_, {id}) => client.movie.findUnique({where: {id}})
-  },
-
-  Mutation: {
-    createMovie: (_, {year, title, genre}) => 
-      client.movie.create({ 
-        data: {
-          year,
-          title,
-          genre
-        }
-      }),
-    deleteMovie: (_, {id}) => client.movie.delete({where: {id}}),
-    updateMovie: (_, {id, year}) => client.movie.update({ where: {id} , data: {year} })
-  }
-}
+import { ApolloServer } from "apollo-server";
+import schema from './schema';
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers
+  schema
 });
 
-server.listen().then(() => console.log("Server is running on http://localhost:4000/"));
+server
+  .listen()
+  .then(() => console.log("Server is running on http://localhost:4000/"));
