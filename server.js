2b3570c1a4baa787a75860d947cf6a571f449182
junghyuni <anjjang23@gmail.com>
2021-04-30 15:18:38 +0900

    prisma client

--- server.js
+++ server.js
@@ -1,38 +1,47 @@
+import { PrismaClient } from ".prisma/client";
 import { ApolloServer, gql } from "apollo-server";
+
+const client = new PrismaClient()
 
 const typeDefs = gql`
   type Movie {
-    id: Int
-    title: String
-    year: Int
+    id: Int!
+    year: Int!
+    title: String!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
   }
 
   type Query {
     movies: [Movie]
-    movie: Movie
+    movie(id:Int!): Movie
   }
 
   type Mutation {
-    createMovie(title: String!): Boolean
-    deleteMovie(title: String!): Boolean
+    createMovie(title: String!, year: Int!, genre: String): Movie
+    deleteMovie(id: Int!): Movie
+    updateMovie(id: Int!, year: Int!): Movie
   }
 `;
 
 const resolvers = {
   Query: {
-    movies: () => [],
-    movie: () => ({ title: "Hello", year: 2021 })
+    movies: () => client.movie.findMany(),
+    movie: (_, {id}) => client.movie.findUnique({where: {id}})
   },
 
   Mutation: {
-    createMovie: (_, {title}) => {
-      console.log(title);
-      return true
-    },
-    deleteMovie: (_, {title}) => {
-      console.log(title);
-      return true
-    }
+    createMovie: (_, {year, title, genre}) => 
+      client.movie.create({ 
+        data: {
+          year,
+          title,
+          genre
+        }
+      }),
+    deleteMovie: (_, {id}) => client.movie.delete({where: {id}}),
+    updateMovie: (_, {id, year}) => client.movie.update({ where: {id} , data: {year} })
   }
 }
 
