3e128cdb616111de4aea2d0cf567ec4067e93196
junghyuni <anjjang23@gmail.com>
2021-05-02 18:47:07 +0900

    see profile

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -8,32 +8,38 @@ export default {
       { firstName, lastName, username, email, password }
     ) => {
       // check if username or email are already on DB.
-      const existingUser = await client.user.findFirst({
-        where: {
-          OR: [
-            {
-              username,
-            },
-            {
-              email,
-            },
-          ],
-        },
-      });
-      console.log(existingUser);
-      // hash password
-      // save and return the user
-      const uglyPassword = await bcrypt.hash(password, 10);
-      
-      return client.user.create({
-        data: {
-          username,
-          email,
-          firstName,
-          lastName,
-          password: uglyPassword,
-        },
-      });
+      try {
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                username,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        if (existingUser) {
+          throw new Error("This username/password is already taken.");
+        }
+        // hash password
+        // save and return the user
+        const uglyPassword = await bcrypt.hash(password, 10);
+        
+        return client.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+      } catch (e) {
+        return e;
+      }
     },
   },
 };
