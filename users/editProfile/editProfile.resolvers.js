c1213a20832df31bb1d68388bfc0eccd67992bdd
junghyuni <anjjang23@gmail.com>
2021-05-03 11:05:39 +0900

    authentication

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,12 +1,15 @@
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'
 import client from "../../client"
 
 export default {
   Mutation: {
     editProfile: async (
       _, 
-      { firstName, lastName, username, email, password: newPassword }
+      { firstName, lastName, username, email, password: newPassword, token }
     ) => { 
+      const { id } = await jwt.verify(token, process.env.SECRET_KEY)
+
       let uglyPassword = null;
 
       if (newPassword) {
@@ -15,7 +18,7 @@ export default {
 
       const updatedUser = await client.user.update({ 
         where: {
-          id: 1
+          id
         }, 
         data: {
           firstName, 
