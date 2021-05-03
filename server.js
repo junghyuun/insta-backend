4e8ad4d271d2058b0522345babcdec3a5fec8f64
junghyuni <anjjang23@gmail.com>
2021-05-03 11:43:19 +0900

    authentication
    send token through context

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ import { ApolloServer } from "apollo-server";
 import schema from './schema';
 
 const server = new ApolloServer({
-  schema
+  schema,
+  context: {
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjIwMDA1ODIyfQ.sKzL2AvRb1E1kus7rygonkv3rT8vFwux7iDvz-cWnUk"
+  }
 });
 
 const PORT = process.env.PORT;
